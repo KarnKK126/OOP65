bf03bd5af09023ff523e8b713ccad19dadcd124c
KarnKaewprapa <88415650+KarnKaewprapa@users.noreply.github.com>
2022-10-11 22:24:38 +0700

    update

---
+++ week8/Lab8/src/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args){
+        Account a1 = new Account(8000, "64070166");
+        new TellerGUI(a1);
+    }
+}
